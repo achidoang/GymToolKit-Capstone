68e7279c262f34d97f27aca4e8cac223ab329192
Arvanda <103157831+arvandza@users.noreply.github.com>
2023-12-22 08:34:19 +0700

    Delete app/build directory

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_dicoding_gymtoolkit_GymApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.dicoding.gymtoolkit.GymApplication"
-)
-public final class _com_dicoding_gymtoolkit_GymApplication {
-}
